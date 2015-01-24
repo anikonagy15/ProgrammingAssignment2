7c45fb705b2ec4df64b7ec348b639d174390c7d1
anikonagy15 <anikonagy15@gmail.com>
2015-01-24 21:46:52 +0100

    added documentations

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
-## Write a short comment describing this function
+# We are using three matrices:
+# x - the matrix that is initialized with makeCacheMatrix()
+# y - the matrix whose inverse is stored in m
+# m - the inverse matrix of y
+# NOTE: 
+# To check if the matrix, whose inverse matrix is stored in m
+# is changed after the inverse was calculated with chaceSolved(), I 
+# introduced y matrix. This matrix is always containing the matrix
+# whose inverse is stored in m.
+# If you reinitialized your object with the set() function, there
+# is no need for this extra check. But if you modify only one element,
+# and don`t recalculate the inverse, it is neccessary to check whose inverse
+# is stored in the cache. 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
-        stored <- NULL
+        y <- NULL
+        # modifies the element of the matrix
         setelement <- function(a, b, c) {
                 x[a,b]<-c
                 x <<- x
         }
-        set <- function(y) {
-                x <<- y
+        # updates the x matrix and makes m null
+        setx <- function(z) {
+                x <<- z
                 m <<- NULL
         }
-        get <- function() x
-        setSolve <- function(solve) {
-                m <<- solve
+        # returns x matrix
+        getx <- function() x
+        # updates m, which is the inverse of y matrix
+        setSolve <- function(r) {
+                m <<- r
         }
+        # returns m that is the inverse of y matrix
         getSolve <- function() m
-        setStored <- function (k) {
-                stored <<- k
+        # updates the y matrix
+        sety <- function (k) {
+                y <<- k
         }
-        getStored <- function () stored
-        list(setelement=setelement, set=set, get=get, 
+        # returns the y matrix
+        gety <- function () y
+        list(setelement=setelement, setx=setx, getx=getx, 
              setSolve=setSolve,
              getSolve=getSolve,
-             setStored=setStored,
-             getStored=getStored)
+             sety=sety,
+             gety=gety)
 }
-## Write a short comment describing this function
+# Calculates and prints the inverse matrix of the initialized matrix.
+# Also updates y accordingly.
 cacheSolve <- function(j, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # puts m into t
         t <- j$getSolve()
-        if(!is.null(t) && identical(j$getStored(), j$get())) {
-                        message("getting cached data")
+        # if m not null and the original matrix is not changed, 
+        # returns with the cached data
+        if(!is.null(t) && identical(j$gety(), j$getx())) {
+                message("getting cached data")
                 return(t)
         }
-        data <- j$get()
+        # if we are here, that means that m was null,
+        # or the original matrix was changed,
+        # let`s calculate the new inverse matrix and stores the new values
+        data <- j$getx()
         t <- solve(data, ...)
         j$setSolve(t)
-        j$setStored(data)
+        j$sety(data)
         t
 }
